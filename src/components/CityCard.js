11e355826ace9515b1ba517a92b7777118e1621e
franklin <franklinbenjamin032@gmail.com>
2022-04-11 18:36:19 -0400

    create CityCard

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const CityCard = (props) => {
+  const { cityImage, cityCountry, cityName} = props;
+  return (
+    <div className ="city-card">
+      <div className="image-cont">
+        <img src={cityImage} />
+      </div>
+      <h4> {cityName}: {cityCountry}</h4>
+    </div>
+  );
+};
+
+export default CityCard;
