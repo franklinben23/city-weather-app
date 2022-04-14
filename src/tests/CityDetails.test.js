37ddd709d54c921f80f93ae7f6dce364388d2650
franklin <franklinbenjamin032@gmail.com>
2022-04-13 22:32:16 -0400

    linter fix

--- src/tests/CityDetails.test.js
+++ src/tests/CityDetails.test.js
@@ -7,22 +7,22 @@ import configureStore from '../redux/configureStore';
 describe('Testing Mission', () => {
   it('Testing Mission component snapshot', () => {
     const cityObj = {
-      cityCountry: "MX",
-      cityImage: "https://www.elsoldemexico.com.mx/metropoli/cdmx/58gppj-cdmx-…ntro%20de%20las%20100%20mejores%20ciudades%20del%20mundo.png",
-      cityName: "Ciudad de Mexico",
-      description: "broken clouds",
+      cityCountry: 'MX',
+      cityImage: 'https://www.elsoldemexico.com.mx/metropoli/cdmx/58gppj-cdmx-…ntro%20de%20las%20100%20mejores%20ciudades%20del%20mundo.png',
+      cityName: 'Ciudad de Mexico',
+      description: 'broken clouds',
       humidity: 22,
-      icon: "http://openweathermap.org/img/wn/04d@2x.png",
+      icon: 'http://openweathermap.org/img/wn/04d@2x.png',
       id: 3530597,
-      mainDesc: "Clouds",
+      mainDesc: 'Clouds',
       pressure: 1016,
       temp: 80.19,
       tempFeelsLike: 78.8,
-      visibility: 10000
-    }
+      visibility: 10000,
+    };
     const testRenderer = TestRenderer.create(
       <Provider store={configureStore}>
-        <CityDetailsM city={cityObj}/>
+        <CityDetailsM city={cityObj} />
       </Provider>,
     ).toJSON();
     expect(testRenderer).toMatchSnapshot();
